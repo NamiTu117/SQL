1865ede49de6dc467bba67523bb57d4458f71107
NamiTu117 <86804169+NamiTu117@users.noreply.github.com>
2021-09-08 15:29:36 -0400

    Updates

--- SQL_ASSIGNMENT(1-6).sql
+++ SQL_ASSIGNMENT(1-6).sql
@@ -1,30 +1,33 @@
 USE db_zoo
 
-/*returns data in habitat table*/
+/*assignment 1*/
 SELECT * FROM tbl_habitat; 
 
-/*retrieves names that have a value of 3*/ 
+/*assignment 2*/ 
 SELECT * FROM tbl_species 
-WHERE species_name='3'; 
+WHERE species_order='3'; 
 
-/*nutrition types 600 or less*/
-SELECT * FROM tbl_nutrition 
-WHERE nutrition_cost <= '600.00';
+/*assignment 3*/
+SELECT nutrition_type 
+FROM tbl_nutrition 
+WHERE nutrition_cost <= 600
 
-/*select ids between 2202 and 2206*/ 
-SELECT * FROM tbl_nutrition
-WHERE nutrition_id BETWEEN 2202 AND 2206; 
+/*assignment 4*/ 
+SELECT species_name
+FROM tbl_species 
+WHERE species_nutrition BETWEEN 2202 and 2206
 
-/*using aliases*/
-SELECT species_name AS Species_Name
-FROM tbl_species; 
-/*end of alias*/
-
-SELECT species_nutrition AS Nutrition_Type 
-FROM tbl_species; 
+select * from tbl_nutrition 
+select * from tbl_species 
 
 
-/*penguin*/
+/*assigment 5*/
+SELECT species_name as 'Species Name', nutrition_type as 'Nutrition Type' 
+FROM tbl_species 
+INNER JOIN tbl_nutrition on tbl_nutrition.nutrition_id = tbl_species.species_nutrition 
+
+
+/*assignment 6*/
 SELECT tbl_specialist.specialist_fname, tbl_specialist.specialist_lname,tbl_specialist.specialist_contact
 FROM tbl_specialist 
 inner join tbl_care on tbl_care.care_specialist = tbl_specialist.specialist_id 
